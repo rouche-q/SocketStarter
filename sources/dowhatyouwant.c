333c869ca2afcf8094f99e042bd5a5f581499432
R3J3CT3D <R3J3CT3D@protonmail.com>
2017-06-03 17:19:03 +0200

    [-] Remove the useless return

--- sources/dowhatyouwant.c
+++ sources/dowhatyouwant.c
@@ -1,11 +1,10 @@
 #include "SocketStarter.h"
 
-void doWhatYouWant(t_server server)
+void 		doWhatYouWant(t_server server)
 {
-	write(server.client_fd, "Hello !\n", strlen("Hello !\n"));
-	write(server.client_fd,
+	write(server.fd_client, "Hello !\n", strlen("Hello !\n"));
+	write(server.fd_client,
 		"You are connected on a server created with SocketStarter !\n",
 		strlen("You are connected on a server created with SocketStarter !\n"));
-	close(server.client_fd);
-	return;
+	close(server.fd_client);
 }
