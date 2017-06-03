1b9977f23fff6d39f315fa1470bb4097cde10b89
R3J3CT3D <R3J3CT3D@protonmail.com>
2017-06-03 17:18:44 +0200

    [~] Just add indentation

--- sources/init.c
+++ sources/init.c
@@ -1,13 +1,12 @@
 #include "SocketStarter.h"
 
-struct sockaddr_in initPortServer(int port)
+struct sockaddr_in 		initPortServer(int port)
 {
-  struct sockaddr_in s_in;
+  struct sockaddr_in 	s_in;
 
   s_in.sin_family = AF_INET;
   s_in.sin_port = htons(port);
   s_in.sin_addr.s_addr = INADDR_ANY;
-
   return (s_in);
 }
 
