6c9191b3978e518fbef3b52dc95be994fb05a463
R3J3CT3D <R3J3CT3D@protonmail.com>
2017-06-03 17:17:33 +0200

    [~] Add an error gestion with perror instead of the "Nop" message

--- sources/socket.c
+++ sources/socket.c
@@ -1,69 +1,67 @@
 #include "SocketStarter.h"
 
-int createSocket()
+int			createSocket()
 {
+	int 						fd;
   struct protoent *pe;
-  int fd;
 
   if (!(pe = getprotobyname("TCP")))
-    return (-1);
+	{
+		perror("getprotobyname");
+		exit(-1);
+	}
   if ((fd = socket(AF_INET, SOCK_STREAM, pe->p_proto)) == -1)
-    return (-1);
-	printf("Created\n");
+	{
+		perror("socket");
+		exit(-1);
+	}
+	printf("Socket created\n");
   return (fd);
 }
 
-
-int bindSocket(int fd, struct sockaddr_in s_in)
+int			bindSocket(int fd, struct sockaddr_in s_in)
 {
   if (bind(fd, (const struct sockaddr *)&s_in, sizeof(s_in)) == -1)
     {
-      printf("Nop\n");
-      if (close(fd) == -1)
-        return (1);
-      return (1);
+			perror("bind");
+			exit(-1);
     }
-  printf("Binded\n");
+  printf("Socket binded\n");
   return (0);
 }
 
-int listenSocket(int fd)
+int			listenSocket(int fd)
 {
   if (listen(fd, 1) == -1)
   {
-		printf("Nop\n");
-    if (close(fd) == -1)
-      return (1);
-    return (1);
+		perror("listen");
+		exit(-1);
   }
 	printf("Listen\n");
   return (0);
 }
 
-int acceptSocket(int fd, struct sockaddr_in s_in_client, socklen_t s_in_size)
+int			acceptSocket(int fd, struct sockaddr_in s_in_client, socklen_t s_in_size)
 {
-  int client_fd;
+  int 	fd_client;
 
-  client_fd = accept(fd, (struct sockaddr *)&s_in_client, &s_in_size);
-  if (client_fd == -1)
+  fd_client = accept(fd, (struct sockaddr *)&s_in_client, &s_in_size);
+  if (fd_client == -1)
     {
-      printf("Nop\n");
-      if (close(fd) == -1)
-        return (1);
-      return (1);
+			perror("accept");
+			exit(-1);
     }
-  printf("Accepted\n");
-  return (client_fd);
+  printf("Client accepted\n");
+  return (fd_client);
 }
 
-int connectSocket(int fd, struct sockaddr_in s_in)
+int			connectSocket(int fd, struct sockaddr_in s_in)
 {
   if (connect(fd, (const struct sockaddr *)&s_in, sizeof(s_in)) == -1)
     {
-      if (close(fd) == -1)
-        return (1);
-      return (1);
+			perror("connect");
+			exit(-1);
     }
-  printf("Connected\n");
+  printf("Client connected\n");
   return (0);
 }
